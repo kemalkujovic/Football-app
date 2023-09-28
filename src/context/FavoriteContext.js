729ddafd52e88c03900c956075f789b730a2fe19
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-09-28 03:43:30 +0200

    fix favorite localStorage

--- src/context/FavoriteContext.js
+++ src/context/FavoriteContext.js
@@ -1,35 +1,51 @@
 import React, { createContext, useEffect, useState } from "react";
 import { leagues } from "../util/helper";
 export const FavoriteContext = createContext();
+
+const getInitalState = () => {
+  const team = localStorage.getItem("team");
+  return team ? JSON.parse(team) : initTeam;
+};
+
+const initTeam = leagues;
+
 export const FavoriteContextProvider = ({ children }) => {
-  const [favorites, setFavorites] = useState([]);
+  const [favorites, setFavorites] = useState(getInitalState);
 
   const addFavorite = (country) => {
-    if (!favorites.includes(country)) {
+    console.log(country);
+    if (
+      !favorites.some((favorite) => favorite.league_id === country.league_id)
+    ) {
       setFavorites((prevFavorites) => [...prevFavorites, country]);
     }
   };
 
   // Funkcija za uklanjanje favorita
   const removeFavorite = (country) => {
-    setFavorites((prevFavorites) =>
-      prevFavorites.filter((fav) => fav !== country)
-    );
+    setFavorites((prevFavorites) => {
+      const newLeague = prevFavorites.filter((fav) => fav !== country);
+      return newLeague;
+    });
   };
-
   useEffect(() => {
-    const storedFavorites = JSON.parse(localStorage.getItem("favorites"));
-    if (storedFavorites && storedFavorites.length > 0) {
-      setFavorites(storedFavorites);
-    }
-    // else {
-    //   setFavorites(leagues);
-    // }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem("favorites", JSON.stringify(favorites));
+    localStorage.setItem("team", JSON.stringify(favorites));
   }, [favorites]);
+
+  // useEffect(() => {
+  //   const storedFavorites = JSON.parse(localStorage.getItem("favorites"));
+  //   if (storedFavorites && storedFavorites.length > 0) {
+  //     setFavorites(storedFavorites);
+  //   }
+
+  //   // else {
+  //   //   setFavorites(leagues);
+  //   // }
+  // }, []);
+
+  // useEffect(() => {
+  //   localStorage.setItem("favorites", JSON.stringify(favorites));
+  // }, [favorites]);
 
   return (
     <FavoriteContext.Provider
