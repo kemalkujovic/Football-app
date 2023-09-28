ad71720222f13c5a7a485da0d5a8eefd8609d7c3
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-09-28 05:07:19 +0200

    slice total players && fix error

--- src/components/TablePlayers/TablePlayers.jsx
+++ src/components/TablePlayers/TablePlayers.jsx
@@ -10,44 +10,52 @@ import classes from "../Table/Table.module.css";
 import { useSelector } from "react-redux";
 const TablePlayers = () => {
   const selector = useSelector((state) => state.football.leagueTopScores);
+  let data = selector;
+  if (selector.length > 0) {
+    data = selector?.slice(0, 10);
+  }
   return (
     <>
-      <TableContainer component={Paper} className={classes.mainContainer}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>#</TableCell>
-              <TableCell>Name</TableCell>
-              <TableCell align="left">TEAM</TableCell>
-              <TableCell align="right">G</TableCell>
-              <TableCell align="right">A</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {selector.length > 0 &&
-              selector?.map((row, index) => (
-                <TableRow
-                  key={index}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row" width="5px">
-                    {row.player_place}
-                  </TableCell>
-                  <TableCell component="th" scope="row">
-                    {row.player_name}
-                  </TableCell>
-                  <TableCell align="left">{row.team_name}</TableCell>
-                  <TableCell align="right" width="5px">
-                    {row.goals}
-                  </TableCell>
-                  <TableCell align="right" width="5px">
-                    {row.assists.length === 0 ? "0" : row.assists}
-                  </TableCell>
-                </TableRow>
-              ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      {selector?.error === 404 ? (
+        <p>No TopScores Data</p>
+      ) : (
+        <TableContainer component={Paper} className={classes.mainContainer}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>#</TableCell>
+                <TableCell>Name</TableCell>
+                <TableCell align="left">TEAM</TableCell>
+                <TableCell align="right">G</TableCell>
+                <TableCell align="right">A</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {selector.length > 0 &&
+                data?.map((row, index) => (
+                  <TableRow
+                    key={index}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row" width="5px">
+                      {row.player_place}
+                    </TableCell>
+                    <TableCell component="th" scope="row">
+                      {row.player_name}
+                    </TableCell>
+                    <TableCell align="left">{row.team_name}</TableCell>
+                    <TableCell align="right" width="5px">
+                      {row.goals}
+                    </TableCell>
+                    <TableCell align="right" width="5px">
+                      {row.assists.length === 0 ? "0" : row.assists}
+                    </TableCell>
+                  </TableRow>
+                ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
     </>
   );
 };
